294c16ba774a1d7d94debe162912c5f4137e474f
arose13 <anthony_rose12@hotmail.com>
2020-01-29 14:35:31 -0500

    sexy unicode

--- rosey-keras/metrics.py
+++ rosey-keras/metrics.py
@@ -5,7 +5,7 @@ import tensorflow.keras.backend as K
 def keras_frac_var_unexplained(y_true, y_pred):
     """
     Compute the unexplained variance
-    SumSq(y - y_hat) / SumSq(y - y_mean)
+    Σ((y - y_hat)^2) / Σ((y - y_mean)^2)
     """
     return K.sum(K.square(y_true - y_pred)) / K.sum(K.square(y_true - K.mean(y_true)))
 
