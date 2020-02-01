364e6fa6508a3d0c55de898f8e4d97b201fd48d1
arose13 <anthony_rose12@hotmail.com>
2020-01-31 23:06:59 -0500

    removed the stupid keras from the name

--- rosey-keras/metrics.py
+++ rosey-keras/metrics.py
@@ -2,7 +2,7 @@ import tensorflow.keras as k
 import tensorflow.keras.backend as K
 
 
-def keras_frac_var_unexplained(y_true, y_pred):
+def fraction_var_unexplained(y_true, y_pred):
     """
     Compute the unexplained variance
     SumSq(y - y_hat) / SumSq(y - y_mean)
@@ -10,8 +10,8 @@ def keras_frac_var_unexplained(y_true, y_pred):
     return K.sum(K.square(y_true - y_pred)) / K.sum(K.square(y_true - K.mean(y_true)))
 
 
-def keras_r2(y_true, y_pred):
+def r2_score(y_true, y_pred):
     """
     Computes the Rsq for Keras models
     """
-    return 1 - keras_frac_var_unexplained(y_true, y_pred)
+    return 1 - fraction_var_unexplained(y_true, y_pred)
