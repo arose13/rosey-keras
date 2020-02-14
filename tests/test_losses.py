c09a676a8709d7adc977ddea2eef0e3c535bd0b3
arose13 <anthony_rose12@hotmail.com>
2020-02-14 14:41:55 -0500

    sexier

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -5,13 +5,13 @@ from rosey_keras.losses import _wasserstein_distance
 
 def test_wasserstein():
     different = _wasserstein_distance(
-        tf.convert_to_tensor([3., 0., 1]),
-        tf.convert_to_tensor([5., 6., 8.])
+        tf.convert_to_tensor([3, 0, 1], dtype='float'),
+        tf.convert_to_tensor([5, 6, 8], dtype='float')
     ).numpy()
     assert different == 5
 
     identical = _wasserstein_distance(
-        tf.convert_to_tensor([3., 0., 1]),
-        tf.convert_to_tensor([0., 1., 3.])
+        tf.convert_to_tensor([3, 0, 1], dtype='float'),
+        tf.convert_to_tensor([5, 6, 8], dtype='float')
     ).numpy()
     assert identical == 0.0
