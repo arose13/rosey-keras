6921ae921d75a2bec3872b1bc1149e546dd9f23a
arose13 <anthony_rose12@hotmail.com>
2020-02-22 18:53:07 -0500

    typo on the test

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -11,7 +11,7 @@ def test_wasserstein():
     assert different == 5
 
     identical = _wasserstein_distance(
-        tf.convert_to_tensor([3, 0, 1], dtype='float'),
-        tf.convert_to_tensor([5, 6, 8], dtype='float')
+        tf.convert_to_tensor([3, 0, 1, 5, 6, 8], dtype='float'),
+        tf.convert_to_tensor([5, 6, 8, 3, 0, 1], dtype='float')
     ).numpy()
     assert identical == 0.0
